78f517f6e715095c02a80687c85f3c70469ba88c
Setholala <Sethedia40@gmail.com>
2025-03-23 16:01:07 -0700

    final

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required
-from .models import Article
+from .models import Book
+from .forms import ExampleForm
 
 @permission_required('yourapp.can_view', raise_exception=True)
 def book_list(request):
@@ -31,3 +32,16 @@ def book_delete(request, pk):
     book = get_object_or_404(Article, pk=pk)
     book.delete()
     return redirect('book_list')
+
+def example_view(request):
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            # Process the form data
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            return render(request, 'bookshelf/form_example.html', {'form': form, 'success': True})
+    else:
+        form = ExampleForm()
+
+    return render(request, 'bookshelf/form_example.html', {'form': form})
